42a7bf3efc41857f4866b75044d5109a29604aa5
Samuel Serapion <samuel.serapion@gmail.com>
2008-07-27 13:42:46 +0000

    Fix Build.
    svn path=/branches/ros-amd64-bringup/; revision=34845

--- reactos/include/psdk/winnt.h
+++ reactos/include/psdk/winnt.h
@@ -2381,13 +2381,6 @@ typedef struct _KNONVOLATILE_CONTEXT_POINTERS {
     };
 } KNONVOLATILE_CONTEXT_POINTERS, *PKNONVOLATILE_CONTEXT_POINTERS;
 
-typedef EXCEPTION_DISPOSITION (*PEXCEPTION_ROUTINE) (
-    IN struct _EXCEPTION_RECORD *ExceptionRecord,
-    IN PVOID EstablisherFrame,
-    IN OUT struct _CONTEXT *ContextRecord,
-    IN OUT PVOID DispatcherContext
-    );
-
 #define UNW_FLAG_NHANDLER 0x0 /* No handler. */
 #define UNW_FLAG_EHANDLER 0x1 /* Exception handler should be called */
 #define UNW_FLAG_UHANDLER 0x2 /* Termination handler that should be called when unwinding an exception */
@@ -2433,17 +2426,6 @@ NTSYSAPI
 BOOLEAN
 __cdecl
 RtlDeleteFunctionTable(PRUNTIME_FUNCTION FunctionTable);
-
-PEXCEPTION_ROUTINE
-WINAPI
-RtlVirtualUnwind (IN ULONG HandlerType,
-                  IN ULONG64 ImageBase,
-                  IN ULONG64 ControlPc,
-                  IN PRUNTIME_FUNCTION FunctionEntry,
-                  IN OUT PCONTEXT ContextRecord,
-                  OUT PVOID *HandlerData,
-                  OUT PULONG64 EstablisherFrame,
-                  IN OUT PKNONVOLATILE_CONTEXT_POINTERS ContextPointers OPTIONAL);
 
 
 typedef struct _KNONVOLATILE_CONTEXT_POINTERS {
