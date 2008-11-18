21d89fb855f5893ec37bbd930e0e79d6719e5047
Timo Kreuzer <timo.kreuzer@reactos.org>
2008-11-18 02:25:41 +0000

    cleanup ntoskrnl.exe: - Move the last stubs into better places - implement KeQueryActiveProcessors, KeInvalidateAllCaches, KeGetRecommendedSharedDataAlignment, KeSaveStateForHibernate, KeSetDmaIoCoherency, PsGetContext, PsSetContext based on x86 implementations. - remove KdbpGetCommandLine from kd.c where it wasn't supposed to be - remove amd64stubs.c
    svn path=/branches/ros-amd64-bringup/; revision=37430
    
    # Conflicts:
    #	reactos/ntoskrnl/amd64stubs.c

--- reactos/ntoskrnl/ke/amd64/cpu.c
+++ reactos/ntoskrnl/ke/amd64/cpu.c
@@ -3,7 +3,8 @@
  * LICENSE:         GPL - See COPYING in the top level directory
  * FILE:            ntoskrnl/ke/amd64/cpu.c
  * PURPOSE:         Routines for CPU-level support
- * PROGRAMMERS:     Timo Kreuzer (timo.kreuzer@reactos.org)
+ * PROGRAMMERS:     Alex Ionescu (alex.ionescu@reactos.org)
+ *                  Timo Kreuzer (timo.kreuzer@reactos.org)
  */
 
 /* INCLUDES *****************************************************************/
@@ -24,9 +25,18 @@ UCHAR KiDoubleFaultTSS[KTSS_IO_MAPS];
 UCHAR KiNMITSS[KTSS_IO_MAPS];
 
 /* CPU Features and Flags */
+ULONG KeProcessorArchitecture;
+ULONG KeProcessorLevel;
+ULONG KeProcessorRevision;
+ULONG KeFeatureBits;
 ULONG KeI386MachineType;
 
+ULONG KeLargestCacheLine = 0x40;
+ULONG KiDmaIoCoherency = 0;
 CHAR KeNumberProcessors = 0;
+KAFFINITY KeActiveProcessors = 1;
+BOOLEAN KiI386PentiumLockErrataPresent;
+BOOLEAN KiSMTProcessorsPresent;
 
 /* FUNCTIONS *****************************************************************/
 
@@ -56,4 +66,84 @@ KiInitializeMachineType(VOID)
     KeI386MachineType = KeLoaderBlock->u.I386.MachineType & 0x000FF;
 }
 
+VOID
+NTAPI
+KeFlushEntireTb(IN BOOLEAN Invalid,
+                IN BOOLEAN AllProcessors)
+{
+    UNIMPLEMENTED;
+}
 
+KAFFINITY
+NTAPI
+KeQueryActiveProcessors(VOID)
+{
+    PAGED_CODE();
+
+    /* Simply return the number of active processors */
+    return KeActiveProcessors;
+}
+
+NTSTATUS
+NTAPI
+KeSaveFloatingPointState(OUT PKFLOATING_SAVE Save)
+{
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+NTSTATUS
+NTAPI
+KeRestoreFloatingPointState(IN PKFLOATING_SAVE Save)
+{
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+BOOLEAN
+NTAPI
+KeInvalidateAllCaches(VOID)
+{
+    /* Only supported on Pentium Pro and higher */
+    if (KeI386CpuType < 6) return FALSE;
+
+    /* Invalidate all caches */
+    __wbinvd();
+    return TRUE;
+}
+
+/*
+ * @implemented
+ */
+ULONG
+NTAPI
+KeGetRecommendedSharedDataAlignment(VOID)
+{
+    /* Return the global variable */
+    return KeLargestCacheLine;
+}
+
+/*
+ * @implemented
+ */
+VOID
+__cdecl
+KeSaveStateForHibernate(IN PKPROCESSOR_STATE State)
+{
+    /* Capture the context */
+    RtlCaptureContext(&State->ContextFrame);
+
+    /* Capture the control state */
+    KiSaveProcessorControlState(State);
+}
+
+/*
+ * @implemented
+ */
+VOID
+NTAPI
+KeSetDmaIoCoherency(IN ULONG Coherency)
+{
+    /* Save the coherency globally */
+    KiDmaIoCoherency = Coherency;
+}
