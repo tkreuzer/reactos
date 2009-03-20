ab06b68ac0713bcbf4b36563bace5c1ca1a43ee9
Samuel Serapion <samuel.serapion@gmail.com>
2009-03-20 04:42:55 +0000

    Implement generic forms of __wine_push_frame and __wine_pop_frame (from wine). This code is duplicated but looks different, specifically the use of Frame->Next and Frame->Prev, this is just an effect of different definitions for EXCEPTION_REGISTRATION_RECORD both Next and Prev are actually the same. Maybe someone could look into tidying this up a bit.
    svn path=/branches/ros-amd64-bringup/; revision=40114

--- reactos/lib/sdk/crt/include/internal/wine/cppexcept.h
+++ reactos/lib/sdk/crt/include/internal/wine/cppexcept.h
@@ -52,7 +52,6 @@
 #define EH_STACK_INVALID    0x08
 #define EH_NESTED_CALL      0x10
 
-#ifdef _M_IX86
 
 #ifdef _MSC_VER
 #pragma warning(push)
@@ -61,15 +60,28 @@
 
 static inline EXCEPTION_REGISTRATION_RECORD *__wine_push_frame( EXCEPTION_REGISTRATION_RECORD *frame )
 {
+#if defined(__i386__)
     frame->Next = (struct _EXCEPTION_REGISTRATION_RECORD *)__readfsdword(0);
 	__writefsdword(0, (unsigned long)frame);
     return frame->Next;
+#else
+	NT_TIB *teb = (NT_TIB *)NtCurrentTeb();
+	frame->Next = teb->ExceptionList;
+	teb->ExceptionList = frame;
+	return frame->Next;
+#endif
 }
 
 static inline EXCEPTION_REGISTRATION_RECORD *__wine_pop_frame( EXCEPTION_REGISTRATION_RECORD *frame )
 {
+#if defined(__i386__)
 	__writefsdword(0, (unsigned long)frame->Next);
     return frame->Next;
+#else
+	NT_TIB *teb = (NT_TIB *)NtCurrentTeb();
+	teb->ExceptionList = frame->Next;
+	return frame->Next;
+#endif
 }
 
 #ifdef _MSC_VER
