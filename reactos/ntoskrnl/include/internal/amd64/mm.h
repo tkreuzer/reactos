c07904d36d1d17429285cfde60feacc2f212d8d7
Timo Kreuzer <timo.kreuzer@reactos.org>
2009-11-15 16:10:52 +0000

    - Get rid of the MmGlobalKernelPageDirectory hack. It's not going to be used on amd64. Instead set up a total of 256 (1MB) shared PDPs in the PML4. - Partly implement phase 1 of MmArmInitSystem - Fix MiPteToAddress - Fix MmSystemRangeStart
    svn path=/branches/ros-amd64-bringup/; revision=44174
    
    # Conflicts:
    #	reactos/ntoskrnl/include/internal/amd64/mm.h
    #	reactos/ntoskrnl/mm/amd64/page.c

--- reactos/ntoskrnl/include/internal/amd64/mm.h
+++ reactos/ntoskrnl/include/internal/amd64/mm.h
@@ -17,15 +17,15 @@ PULONG_PTR MmGetPageDirectory(VOID);
 #define HYPER_SPACE_END                        0xFFFFF77FFFFFFFFFULL
 #define MI_SESSION_VIEW_END             (PVOID)0xFFFFF97FFF000000ULL
 #define MI_SESSION_SPACE_END            (PVOID)0xFFFFF98000000000ULL
-
+#define MI_SYSTEM_PTE_START             (PVOID)0xFFFFFAA000000000ULL
 #define MI_PAGED_POOL_START             (PVOID)0xFFFFFA8000000000ULL
-
 #define MI_NON_PAGED_SYSTEM_START_MIN          0xFFFFFAA000000000ULL
 #define MI_NONPAGED_POOL_END            (PVOID)0xFFFFFAE000000000ULL
-
 #define MI_DEBUG_MAPPING                (PVOID)0xFFFFFFFF80000000ULL // FIXME
+#define MI_HIGHEST_SYSTEM_ADDRESS       (PVOID)0xFFFFFFFFFFFFFFFFULL
 
 
+#define MI_NUMBER_SYSTEM_PTES 22000
 
 PULONG64
 FORCEINLINE
@@ -70,9 +70,44 @@ MiAddressToPte(PVOID Address)
     return (PMMPTE)(PTE_BASE + Offset);
 }
 
+/* Convert a PTE into a corresponding address */
+PVOID
+FORCEINLINE
+MiPteToAddress(PMMPTE Pte)
+{
+    /* Use signed math */
+    LONG64 Temp = (LONG64)Pte;
+    Temp <<= 25;
+    Temp >>= 16;
+    return (PVOID)Temp;
+}
+
 #define ADDR_TO_PAGE_TABLE(v) (((ULONG)(v)) / (1024 * PAGE_SIZE))
 #define ADDR_TO_PDE_OFFSET(v) ((((ULONG)(v)) / (1024 * PAGE_SIZE)))
 #define ADDR_TO_PTE_OFFSET(v)  ((((ULONG)(v)) % (1024 * PAGE_SIZE)) / PAGE_SIZE)
+
+#define VAtoPXI(va) ((((ULONG64)va) >> PXI_SHIFT) & 0x1FF)
+#define VAtoPPI(va) ((((ULONG64)va) >> PPI_SHIFT) & 0x1FF)
+#define VAtoPDI(va) ((((ULONG64)va) >> PDI_SHIFT) & 0x1FF)
+#define VAtoPTI(va) ((((ULONG64)va) >> PTI_SHIFT) & 0x1FF)
+
+/* We don't use these hacks */
+VOID
+FORCEINLINE
+MmUpdatePageDir(PEPROCESS Process, PVOID Address, ULONG Size)
+{
+    /* Nothing to do */
+}
+
+VOID
+FORCEINLINE
+MmInitGlobalKernelPageDirectory(VOID)
+{
+    /* Nothing to do */
+}
+
+
+/// MIARM.H 
 
 /* Easy accessing PFN in PTE */
 #define PFN_FROM_PTE(v) ((v)->u.Hard.PageFrameNumber)
