05245bae22b93afa413d201756c103622fd0ad72
Timo Kreuzer <timo.kreuzer@reactos.org>
2009-11-22 19:16:50 +0000

    [MM] Use an array of PFN_NUMBER to pass to MiMapPagesToZeroInHyperSpace instead of MMPFNs which were then converted to PFN_NUMBERs.
    svn path=/branches/ros-amd64-bringup/; revision=44268

--- reactos/ntoskrnl/include/internal/mm.h
+++ reactos/ntoskrnl/include/internal/mm.h
@@ -1190,7 +1190,7 @@ MiUnmapPageInHyperSpace(IN PEPROCESS Process,
 
 PVOID
 NTAPI
-MiMapPagesToZeroInHyperSpace(IN PMMPFN *Pages,
+MiMapPagesToZeroInHyperSpace(IN PFN_NUMBER *Pages,
                              IN PFN_NUMBER NumberOfPages);
 
 VOID
@@ -1213,8 +1213,7 @@ FORCEINLINE
 PVOID
 MiMapPageToZeroInHyperSpace(IN PFN_NUMBER Page)
 {
-    PMMPFN Pfn1 = MiGetPfnEntry(Page);
-    return MiMapPagesToZeroInHyperSpace(&Pfn1, 1);
+    return MiMapPagesToZeroInHyperSpace(&Page, 1);
 }
 
 #define MmDeleteHyperspaceMapping(x) MiUnmapPageInHyperSpace(HyperProcess, x, HyperIrql);
