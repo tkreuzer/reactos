83b3a359dfca89c23af48c7c159098a3124a0a9e
Samuel Serapion <samuel.serapion@gmail.com>
2009-12-24 04:12:02 +0000

    [ntoskrnl] - enable KdpEnableSafeMem for amd64 - unhack ExInitPoolLookasidePointers - hackplement KiInitializeContextThread - Remove extended eflag code from kdb, its useless. - Rewrite KdbpCmdGdtLdtIdt - Enable KdbpCmdTss - misc kdb fixes.
    svn path=/branches/ros-amd64-bringup/; revision=44749

--- reactos/ntoskrnl/ex/lookas.c
+++ reactos/ntoskrnl/ex/lookas.c
@@ -72,10 +72,6 @@ ExInitPoolLookasidePointers(VOID)
         InitializeSListHead(&Entry->ListHead);
 
         /* Bind to PRCB */
-#ifdef _M_AMD64
-    DPRINT1("Something is missing here, Prcb = %p\n", Prcb);
- // FIXME
-#else
         Prcb->PPNPagedLookasideList[i].P = Entry;
         Prcb->PPNPagedLookasideList[i].L = Entry;
 
