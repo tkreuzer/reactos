4da9ed2ecd689fb4939e38b20c7f1ed4d9b8b17f
Timo Kreuzer <timo.kreuzer@reactos.org>
2015-12-06 17:22:58 +0100

    [TCPIP] Fix formatting

--- drivers/network/tcpip/tcpip/info.c
+++ drivers/network/tcpip/tcpip/info.c
@@ -278,23 +278,31 @@ TDI_STATUS InfoTdiQueryInformationEx(
                      return TDI_INVALID_PARAMETER;
 
                  if (ID->toi_entity.tei_entity == IF_ENTITY)
+                 {
                      if ((EntityListContext = GetContext(ID->toi_entity)))
                          return InfoTdiQueryGetInterfaceMIB(ID->toi_entity, EntityListContext, Buffer, BufferSize);
                      else
                          return TDI_INVALID_PARAMETER;
+                 }
                  else if (ID->toi_entity.tei_entity == CL_NL_ENTITY ||
                           ID->toi_entity.tei_entity == CO_NL_ENTITY)
+                 {
                      if ((EntityListContext = GetContext(ID->toi_entity)))
                          return InfoTdiQueryGetIPSnmpInfo(ID->toi_entity, EntityListContext, Buffer, BufferSize);
                      else
                          return TDI_INVALID_PARAMETER;
+                 }
                  else if (ID->toi_entity.tei_entity == AT_ENTITY)
+                 {
                      if ((EntityListContext = GetContext(ID->toi_entity)))
                          return InfoTdiQueryGetATInfo(ID->toi_entity, EntityListContext, Buffer, BufferSize);
                      else
                          return TDI_INVALID_PARAMETER;
+                 }
                  else
+                 {
                      return TDI_INVALID_PARAMETER;
+                 }
 
               case IP_MIB_ADDRTABLE_ENTRY_ID:
                  if (ID->toi_type != INFO_TYPE_PROVIDER)
@@ -302,65 +310,89 @@ TDI_STATUS InfoTdiQueryInformationEx(
 
                  if (ID->toi_entity.tei_entity == CL_NL_ENTITY ||
                      ID->toi_entity.tei_entity == CO_NL_ENTITY)
-                    return InfoTdiQueryGetAddrTable(ID->toi_entity, Buffer, BufferSize);
-                else if (ID->toi_entity.tei_entity == CO_TL_ENTITY)
+                 {
+                     return InfoTdiQueryGetAddrTable(ID->toi_entity, Buffer, BufferSize);
+                 }
+                 else if (ID->toi_entity.tei_entity == CO_TL_ENTITY)
+                 {
                      if ((EntityListContext = GetContext(ID->toi_entity)))
                          return InfoTdiQueryGetConnectionTcpTable(EntityListContext, Buffer, BufferSize, TcpUdpClassOwnerPid);
                      else
                          return TDI_INVALID_PARAMETER;
-                else if (ID->toi_entity.tei_entity == CL_TL_ENTITY)
+                 }
+                 else if (ID->toi_entity.tei_entity == CL_TL_ENTITY)
+                 {
                      if ((EntityListContext = GetContext(ID->toi_entity)))
                          return InfoTdiQueryGetConnectionUdpTable(EntityListContext, Buffer, BufferSize, TcpUdpClassOwnerPid);
                      else
                          return TDI_INVALID_PARAMETER;
-                else
+                 }
+                 else
+                 {
                     return TDI_INVALID_PARAMETER;
+                 }
 
               case IP_MIB_ARPTABLE_ENTRY_ID:
                  if (ID->toi_type != INFO_TYPE_PROVIDER)
                      return TDI_INVALID_PARAMETER;
 
                  if (ID->toi_entity.tei_entity == AT_ENTITY)
+                 {
                      if ((EntityListContext = GetContext(ID->toi_entity)))
                          return InfoTdiQueryGetArptableMIB(ID->toi_entity, EntityListContext,
                                                            Buffer, BufferSize);
                      else
                          return TDI_INVALID_PARAMETER;
+                 }
                  else if (ID->toi_entity.tei_entity == CO_NL_ENTITY ||
                           ID->toi_entity.tei_entity == CL_NL_ENTITY)
+                 {
                      if ((EntityListContext = GetContext(ID->toi_entity)))
                          return InfoTdiQueryGetRouteTable(EntityListContext, Buffer, BufferSize);
                      else
                          return TDI_INVALID_PARAMETER;
+                 }
                  else if (ID->toi_entity.tei_entity == CO_TL_ENTITY)
+                 {
                      if ((EntityListContext = GetContext(ID->toi_entity)))
                          return InfoTdiQueryGetConnectionTcpTable(EntityListContext, Buffer, BufferSize, TcpUdpClassBasic);
                      else
                          return TDI_INVALID_PARAMETER;
+                 }
                  else if (ID->toi_entity.tei_entity == CL_TL_ENTITY)
+                 {
                      if ((EntityListContext = GetContext(ID->toi_entity)))
                          return InfoTdiQueryGetConnectionUdpTable(EntityListContext, Buffer, BufferSize, TcpUdpClassBasic);
                      else
                          return TDI_INVALID_PARAMETER;
+                 }
                  else
+                 {
                      return TDI_INVALID_PARAMETER;
+                 }
 
               case IP_SPECIFIC_MODULE_ENTRY_ID:
                  if (ID->toi_type != INFO_TYPE_PROVIDER)
                      return TDI_INVALID_PARAMETER;
 
                  if (ID->toi_entity.tei_entity == CO_TL_ENTITY)
+                 {
                      if ((EntityListContext = GetContext(ID->toi_entity)))
                          return InfoTdiQueryGetConnectionTcpTable(EntityListContext, Buffer, BufferSize, TcpUdpClassOwner);
                      else
                          return TDI_INVALID_PARAMETER;
+                 }
                  else if (ID->toi_entity.tei_entity == CL_TL_ENTITY)
+                 {
                      if ((EntityListContext = GetContext(ID->toi_entity)))
                          return InfoTdiQueryGetConnectionUdpTable(EntityListContext, Buffer, BufferSize, TcpUdpClassOwner);
                      else
                          return TDI_INVALID_PARAMETER;
+                 }
                  else
+                 {
                      return TDI_INVALID_PARAMETER;
+                 }
 
 #if 0
               case IP_INTFC_INFO_ID:
@@ -426,19 +458,25 @@ TDI_STATUS InfoTdiSetInformationEx
                                 return TDI_INVALID_PARAMETER;
 
                             if (ID->toi_entity.tei_entity == AT_ENTITY)
+                            {
                                 if ((EntityListContext = GetContext(ID->toi_entity)))
                                     return InfoTdiSetArptableMIB(EntityListContext, Buffer,
                                         BufferSize);
                                 else
                                     return TDI_INVALID_PARAMETER;
+                            }
                             else if (ID->toi_entity.tei_entity == CL_NL_ENTITY
                                 || ID->toi_entity.tei_entity == CO_NL_ENTITY)
+                            {
                                 if ((EntityListContext = GetContext(ID->toi_entity)))
                                     return InfoTdiSetRoute(EntityListContext, Buffer, BufferSize);
                                 else
                                     return TDI_INVALID_PARAMETER;
+                            }
                             else
+                            {
                                 return TDI_INVALID_PARAMETER;
+                            }
 
                         default:
                             return TDI_INVALID_REQUEST;
