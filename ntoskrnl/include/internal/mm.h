792851c104a5fb427f3224e931e5644ca406c3e8
tkreuzer <timo.kreuzer@reactos.org>
2015-12-12 00:28:40 +0100

    Cleanup some stuff

--- ntoskrnl/include/internal/mm.h
+++ ntoskrnl/include/internal/mm.h
@@ -548,15 +548,6 @@ MmFindGap(
     BOOLEAN TopDown
 );
 
-VOID
-NTAPI
-MiRosCheckMemoryAreas(
-   PMMSUPPORT AddressSpace);
-
-VOID
-NTAPI
-MiCheckAllProcessMemoryAreas(VOID);
-
 /* npool.c *******************************************************************/
 
 INIT_FUNCTION
@@ -603,12 +594,6 @@ MmBuildMdlFromPages(
 );
 
 /* mminit.c ******************************************************************/
-
-VOID
-NTAPI
-MmInit1(
-    VOID
-);
 
 INIT_FUNCTION
 BOOLEAN
@@ -714,18 +699,6 @@ NTAPI
 MmSetMemoryPriorityProcess(
     IN PEPROCESS Process,
     IN UCHAR MemoryPriority
-);
-
-/* i386/pfault.c *************************************************************/
-
-NTSTATUS
-NTAPI
-MmPageFault(
-    ULONG Cs,
-    PULONG Eip,
-    PULONG Eax,
-    ULONG Cr2,
-    ULONG ErrorCode
 );
 
 /* special.c *****************************************************************/
