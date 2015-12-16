26defe70a0d553e0183ceaba58d69426c176e62e
tkreuzer <timo.kreuzer@reactos.org>
2015-12-16 19:57:24 +0100

    implement the libe code in DibLib

--- win32ss/gdi/diblib/DibLib_Line.h
+++ win32ss/gdi/diblib/DibLib_Line.h
@@ -1,110 +1,175 @@
 
-#define _DibFunction __DIB_FUNCTION_NAME(__FUNCTIONNAME, _SOURCE_BPP, _DEST_BPP)
+#define _DibLine __DIB_FUNCTION_NAME(__FUNCTIONNAME, _SOURCE_BPP, _DEST_BPP)
+#define _SkipPixels __DIB_FUNCTION_NAME(__PASTE(__FUNCTIONNAME, _SkipPixels) , _SOURCE_BPP, _DEST_BPP)
+
+#define _WritePixel(pj, jShift, c) __PASTE(_WritePixel_, _DEST_BPP)(pj, jShift, c)
+#define _SHIFT(bpp, x) __PASTE(_SHIFT_, bpp)(x)
+
+#ifdef __INVERTED_COORDINATES
+#define _CalcDest(psi, x, y) ((PUCHAR)((psi)->pvScan0) + (x * (psi)->lDelta) + (_DEST_BPP * y / 8))
+#define _NextPixelY(bpp, ppj, pjShift, dy) __PASTE(_NextPixel_, bpp)(ppj, pjShift)
+#define _NextPixelX(bpp, ppj, pjShift)  *(ppj) += pld->siDst.cjAdvanceY
+#define _CALCSHIFT(bpp, pshift, x, y) __PASTE(_CALCSHIFT_, bpp)(pshift, y)
+#else
+#define _CalcDest(psi, x, y) ((PUCHAR)((psi)->pvScan0) + (y * (psi)->lDelta) + (_DEST_BPP * x / 8))
+#define _NextPixelX(bpp, ppj, pjShift) __PASTE(_NextPixel_, bpp)(ppj, pjShift)
+#define _NextPixelY(bpp, ppj, pjShift, dy)  *(ppj) += (dy) * pld->siDst.cjAdvanceY
+#define _CALCSHIFT(bpp, pshift, x, y) __PASTE(_CALCSHIFT_, bpp)(pshift, x)
+#endif
+
+#if 0 && DBG
+#define NT_ASSERT(exp) ((VOID)((!(exp)) ? (__int2c(), FALSE) : TRUE))
+#else
+#define NT_ASSERT(exp)
+#endif
+
+FORCEINLINE
+VOID
+_SkipPixels(
+    LONG cx,
+    PLINEDATA pld,
+    LONG *px,
+    LONG *py,
+    LONG dy,
+    LONG *plRemainder)
+{
+    LONG lDiv, lMod;
+
+    *px += cx;
+
+    lDiv = MulDivMod(cx, pld->lDelta, pld->lRemainder0, &lMod);
+
+    *py += dy * lDiv;
+
+    *plRemainder -= lMod;
+
+    if (*plRemainder < 0)
+    {
+        *py += dy;
+        *plRemainder += pld->lRemainder0;
+    }
+}
 
 VOID
 FASTCALL
-_DibFunction(
-    PLINEDATA pli)
+_DibLine(
+    PLINEDATA pld)
 {
-#if 0
     LONG x, y, xMax, cx, dy;
     ULONG iRun, iStyle;
-    LONG lGamma;
+    LONG lRemainder;
+    PBYTE pjDest, pjDestBase;
+    _SHIFT(_DEST_BPP, BYTE jDstShift;)
+
+    pjDestBase = pld->siDst.pjBase;
+
+    /* Get the y start coordinate */
+    y = pld->runOtherCoordinate.iStart;
+
+    /* Set y advance */
+    dy = (pld->runOtherCoordinate.iStop > y) ? 1 : -1;
+
+    /* Get the initial remainder value */
+    lRemainder = pld->lRemainder;
 
     /* Outer loop to handle multiple runs */
     iRun = 0;
     do
     {
         /* Get the start and end position of this run */
-        x = pli->cl.arun[iRun].iStart;
-        xMax = pli->cl.arun[iRun].iStop;
-
-        /* Get the y start coordinate */
-        y = pli->runOtherCoordinate.iStart;
-
-        /* Set y advance */
-        dy = (pli->runOtherCoordinate.iStop > y) ? 1 : -1;
+        x = pld->cl.arun[iRun].iStart;
+        xMax = pld->cl.arun[iRun].iStop;
 
         /* Check if we have styles */
-        if (pli->cStyles > 0)
+        if (pld->cStyles > 0)
         {
             /* Style count must be even and max 16! */
-            NT_ASSERT((pli->cStyles & 1) == 0);
-            NT_ASSERT(pli->cStyles <= 16);
+            NT_ASSERT((pld->cStyles & 1) == 0);
+            NT_ASSERT(pld->cStyles <= 16);
 
             /* Get the current style index */
-            iStyle = pli->iStyle;
-            NT_ASSERT(iStyle < pli->cStyles);
+            iStyle = pld->iStyle;
+            NT_ASSERT(iStyle < pld->cStyles);
 
             /* Calculate the remaining pixels */
-            cx = pli->acxyStyle[iStyle] - pli->xyStylePos;
+            cx = pld->acxyStyle[iStyle] - pld->xyStylePos;
 
             /* Check if this is a space style step */
             if (iStyle & 1)
             {
                 /* Skip this style index and continue with the next one */
-                SkipPixels(cx, pli, &x, &y, dy, &lGamma);
+                _SkipPixels(cx, pld, &x, &y, dy, &lRemainder);
 
-                iStyle = (iStyle + 1) % pli->cStyles;
+                iStyle = (iStyle + 1) % pld->cStyles;
 
                 /* Get new cx */
-                cx = pli->acxyStyle[iStyle];
+                cx = pld->acxyStyle[iStyle];
             }
 
             xMax = min(xMax, x + cx);
         }
 
-        /* Get the gamma value */
-        lGamma = pli->lGamma;
-
         /* 2nd loop for styles */
         while (TRUE)
         {
+            pjDest = _CalcDest(&pld->siDst, x, y);
+            _CALCSHIFT(_DEST_BPP, &jDstShift, x, y);
+
             /* 3rd loop for the pixels */
             while (x < xMax)
             {
-                /* Draw this pixel */
-                _PutPixel(pli, x, y);
+                /* Write the pixel and go to the next dest pixel */
+                _WritePixel(pjDest, jDstShift, pld->ulColor);
+                _NextPixelX(_DEST_BPP, &pjDest, &jDstShift);
 
-                /* Advance one pixel in x direction */
-                x++;
-
-                lGamma -= pli->lDelta; // lDelta = (ptfxB.y - prfxA.y)
-                if (lGamma < 0)
+                /* Update the remainder and check if went below 0 */
+                lRemainder -= pld->lDelta;
+                if (lRemainder < 0)
                 {
+                    /* We need to go 1 pixel down (or up) */
                     y += dy;
-                    lGamma += pli->lGamma0;
+                    _NextPixelY(_DEST_BPP, &pjDest, &jDstShift, dy);
+
+                    /* Account for this in the remainder */
+                    lRemainder += pld->lRemainder0;
                 }
             }
 
+            x = xMax;
+
             /* If there are no styles, we are done */
-            if (pli->cStyles == 0)
+            if (pld->cStyles == 0)
             {
                 break;
             }
 
             /* Skip the next (odd, i.e. NULL) style index */
-            cx = pli->acxyStyle[iStyle + 1];
+            cx = pld->acxyStyle[iStyle + 1];
 
             /* Check if that's still within the current run */
-            if (x + cx > pli->cl.arun[iRun].iStop)
+            if (x + cx > pld->cl.arun[iRun].iStop)
             {
                 break;
             }
 
             /*  */
-            SkipPixels(cx, pli, &x, &y, dy, &lGamma);
+            _SkipPixels(cx, pld, &x, &y, dy, &lRemainder);
 
             /* Advance by 2 styles */
-            iStyle = (iStyle + 2) % pli->cStyles;
+            iStyle = (iStyle + 2) % pld->cStyles;
 
             /* Recalculate xMax */
-            xMax = min(pli->cl.arun[iRun].iStop, x + pli->acxyStyle[iStyle]);
+            xMax = min(pld->cl.arun[iRun].iStop, x + pld->acxyStyle[iStyle]);
         }
 
         iRun++;
 
-    } while (iRun < pli->cl.c);
-#endif
+    } while (iRun < pld->cl.c);
 }
 
+#undef _DibLine
+#undef _CalcDest
+#undef _NextPixelX
+#undef _NextPixelY
+#undef _CALCSHIFT
+#undef __FUNCTIONNAME2
--- win32ss/gdi/diblib/Line.c
+++ win32ss/gdi/diblib/Line.c
@@ -1,11 +1,12 @@
 
 #include "DibLib.h"
 
-#define __USES_SOURCE -1 // HACK
+#define __USES_SOURCE 0
 #define __USES_PATTERN 1
 #define __USES_DEST 1
 #define __USES_MASK 0
 
+#define PFN_DIBFUNCTION PFN_DIB_LINETO
 #define __HEADER_NAME "DibLib_Line.h"
 #define __FUNCTIONNAME Line_MajorX
 
