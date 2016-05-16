fd6bece214c49dfec2a2aba00320de2721349ffd
tkreuzer <timo.kreuzer@reactos.org>
2016-05-16 09:01:58 +0200

    [NTOS_NEW:Rtl] Implement ByteSwap functions

--- ntoskrnl_new/Rtl/ByteSwap.cpp
+++ ntoskrnl_new/Rtl/ByteSwap.cpp
@@ -8,8 +8,11 @@ FASTCALL
 RtlUshortByteSwap (
     _In_ USHORT Source)
 {
-    __debugbreak();
-    return 0;
+#if defined(_M_IX86) || (defined(_M_AMD64) || defined(_M_IA64)) || defined(_M_ARM) || defined(_M_ARM64)
+    return _byteswap_ushort(Source);
+#else
+    return (Source >> 8) | (Source << 8);
+#endif // _M_IX86
 }
 
 ULONG
@@ -17,8 +20,14 @@ FASTCALL
 RtlUlongByteSwap (
     _In_ ULONG Source)
 {
-    __debugbreak();
-    return 0;
+#if defined(_M_IX86) || (defined(_M_AMD64) || defined(_M_IA64)) || defined(_M_ARM) || defined(_M_ARM64)
+    return _byteswap_ulong(Source);
+#else
+    return ((Source & 0x000000FF) << 24) |
+           ((Source & 0x0000FF00) << 8) |
+           ((Source & 0x00FF0000) >> 8) |
+           ((Source & 0xFF000000) >> 24);
+#endif // _M_IX86
 }
 
 ULONGLONG
@@ -26,8 +35,11 @@ FASTCALL
 RtlUlonglongByteSwap (
     _In_ ULONGLONG Source)
 {
-    __debugbreak();
-    return 0;
+#if defined(_M_IX86) || (defined(_M_AMD64) || defined(_M_IA64)) || defined(_M_ARM) || defined(_M_ARM64)
+    return _byteswap_uint64(Source);
+#else
+    return RtlUlongByteSwap(Source >> 32) | RtlUlonglongByteSwap(Source & 0xFFFFFFFF) << 32;
+#endif // _M_IX86
 }
 
 } // extern "C"
