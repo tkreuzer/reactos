457043782ef3ec654d1203d4f6087cf7e8cdfb3b
tkreuzer <timo.kreuzer@reactos.org>
2016-10-24 02:05:17 +0200

    [NTDLL] i386 dispatchc with asm comments

--- reactos/dll/ntdll/dispatch/i386/dispatch.c
+++ reactos/dll/ntdll/dispatch/i386/dispatch.c
@@ -9,21 +9,30 @@ KiUserApcDispatcher(
 {
 
     /* Setup SEH stack */
+    //lea eax, [esp+CONTEXT_ALIGNED_SIZE+16]
+    //mov ecx, fs:[TEB_EXCEPTION_LIST]
+    //mov [eax], ecx
     SehRecord.Next = __readfsdword(FIELD_OFFSET(TEB, ExceptionList));
+
+    //mov edx, offset _KiUserApcExceptionHandler
+    //mov [eax+4], edx
     SehRecord.Handler = KiUserApcExceptionHandler;
 
     /* Enable SEH */
+    //mov fs:[TEB_EXCEPTION_LIST], eax
     _writefsdword(FIELD_OFFSET(TEB, ExceptionList), &SehRecord);
 
     /* Put the Context in EDI */
-    pop eax
-
+    //pop eax
+    //lea Context, [esp+12]
     Context = &[esp+12]
 
     /* Call the APC Routine */
     call eax
 
     /* Restore exception list */
+    //mov ecx, [Context+CONTEXT_ALIGNED_SIZE]
+    //mov fs:[TEB_EXCEPTION_LIST], ecx
     _writefsdword(FIELD_OFFSET(TEB, ExceptionList), SehRecord.Next);
 
     /* Switch back to the context */
