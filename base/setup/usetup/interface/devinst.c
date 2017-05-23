dac5926c8ae08e3066b9e1acae42cabc69c27a31
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2017-05-23 22:40:34 +0000

    [USETUP]: Use the newly-introduced CombinePaths and ConcatPaths functions. Fix also few comments, and place some UNICODE_NULLs here & there.
    svn path=/branches/setup_improvements/; revision=74640

--- base/setup/usetup/interface/devinst.c
+++ base/setup/usetup/interface/devinst.c
@@ -50,7 +50,7 @@ InstallDriver(
     OBJECT_ATTRIBUTES ObjectAttributes;
     HANDLE hService;
     INFCONTEXT Context;
-    LPWSTR Driver, ClassGuid, ImagePath, FullImagePath;
+    LPWSTR Driver, ImagePath, FullImagePath;
     ULONG dwValue;
     ULONG Disposition;
     NTSTATUS Status;
@@ -84,14 +84,14 @@ InstallDriver(
 
     /* Prepare full driver path */
     dwValue = PathPrefix.MaximumLength + wcslen(ImagePath) * sizeof(WCHAR);
-    FullImagePath = (LPWSTR)RtlAllocateHeap(ProcessHeap, 0, dwValue);
+    FullImagePath = (PWSTR)RtlAllocateHeap(ProcessHeap, 0, dwValue);
     if (!FullImagePath)
     {
         DPRINT1("RtlAllocateHeap() failed\n");
         return FALSE;
     }
     RtlCopyMemory(FullImagePath, PathPrefix.Buffer, PathPrefix.MaximumLength);
-    wcscat(FullImagePath, ImagePath);
+    ConcatPaths(FullImagePath, dwValue / sizeof(WCHAR), 1, ImagePath);
 
     DPRINT1("Using driver '%S' for device '%S'\n", ImagePath, DeviceId);
 
