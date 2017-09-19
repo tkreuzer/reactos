c5a10a26fb10faf72a94974060c92ee4a24d3454
tkreuzer <timo.kreuzer@reactos.org>
2017-09-19 09:55:34 +0200

    [WIN32NT_APITEST] Add test for NtGdiExtSelectClipRgn.c

---
+++ rostests/apitests/win32nt/ntgdi/NtGdiExtSelectClipRgn.c
@@ -0,0 +1,13 @@
+/*
+ * PROJECT:         ReactOS api tests
+ * LICENSE:         GPL - See COPYING in the top level directory
+ * PURPOSE:         Test for NtGdiExtSelectClipRgn
+ * PROGRAMMERS:
+ */
+
+#include <win32nt.h>
+
+START_TEST(NtGdiExtSelectClipRgn)
+{
+
+}
