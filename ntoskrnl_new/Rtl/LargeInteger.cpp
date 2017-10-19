f635e9bcb916741b2a48973cf669e96374634fb9
Timo Kreuzer <timo.kreuzer@reactos.org>
2017-10-19 16:50:08 +0200

    [NTOS::Rtl] Implement LargeInteger functions

--- ntoskrnl_new/Rtl/LargeInteger.cpp
+++ ntoskrnl_new/Rtl/LargeInteger.cpp
@@ -12,9 +12,10 @@ RtlLargeIntegerAdd (
     _In_ LARGE_INTEGER Addend2
     )
 {
-    LARGE_INTEGER result = {{0}};
-    __debugbreak();
-    return result;
+    LARGE_INTEGER sum;
+
+    sum.QuadPart = Addend1.QuadPart + Addend2.QuadPart;
+    return sum;
 }
 
 DECLSPEC_DEPRECATED_DDK
@@ -25,9 +26,10 @@ RtlLargeIntegerSubtract (
     _In_ LARGE_INTEGER Minuend,
     _In_ LARGE_INTEGER Subtrahend)
 {
-    LARGE_INTEGER result = {{0}};
-    __debugbreak();
-    return result;
+    LARGE_INTEGER difference;
+
+    difference.QuadPart = Minuend.QuadPart - Subtrahend.QuadPart;
+    return difference;
 }
 
 DECLSPEC_DEPRECATED_DDK
@@ -39,9 +41,15 @@ RtlLargeIntegerDivide (
     _In_ LARGE_INTEGER Divisor,
     _Out_opt_ PLARGE_INTEGER Remainder)
 {
-    LARGE_INTEGER result = {{0}};
-    __debugbreak();
-    return result;
+    LARGE_INTEGER quotient;
+
+    quotient.QuadPart = Dividend.QuadPart / Divisor.QuadPart;
+    if (ARGUMENT_PRESENT(Remainder))
+    {
+        Remainder->QuadPart = Dividend.QuadPart % Divisor.QuadPart;
+    }
+
+    return quotient;
 }
 
 DECLSPEC_DEPRECATED_DDK
@@ -53,8 +61,9 @@ RtlLargeIntegerArithmeticShift (
     _In_ CCHAR ShiftCount
     )
 {
-    LARGE_INTEGER result = {{0}};
-    __debugbreak();
+    LARGE_INTEGER result;
+
+    result.QuadPart = LargeInteger.QuadPart >> ShiftCount;
     return result;
 }
 
@@ -66,8 +75,9 @@ RtlLargeIntegerNegate (
     _In_ LARGE_INTEGER Subtrahend
     )
 {
-    LARGE_INTEGER result = {{0}};
-    __debugbreak();
+    LARGE_INTEGER result;
+
+    result.QuadPart = -Subtrahend.QuadPart;
     return result;
 }
 
@@ -80,8 +90,9 @@ RtlLargeIntegerShiftLeft (
     _In_ CCHAR ShiftCount
     )
 {
-    LARGE_INTEGER result = {{0}};
-    __debugbreak();
+    LARGE_INTEGER result;
+
+    result.QuadPart = LargeInteger.QuadPart << ShiftCount;
     return result;
 }
 
@@ -94,8 +105,9 @@ RtlLargeIntegerShiftRight (
     _In_ CCHAR ShiftCount
     )
 {
-    LARGE_INTEGER result = {{0}};
-    __debugbreak();
+    LARGE_INTEGER result;
+
+    result.QuadPart = (ULONG64)LargeInteger.QuadPart >> ShiftCount;
     return result;
 }
 
@@ -108,9 +120,10 @@ RtlEnlargedIntegerMultiply (
     _In_ LONG Multiplier
     )
 {
-    LARGE_INTEGER result = {{0}};
-    __debugbreak();
-    return result;
+    LARGE_INTEGER product;
+
+    product.QuadPart = (LONG64)Multiplicand * (LONG64)Multiplier;
+    return product;
 }
 
 DECLSPEC_DEPRECATED_DDK
@@ -122,9 +135,10 @@ RtlEnlargedUnsignedMultiply (
     _In_ ULONG Multiplier
     )
 {
-    LARGE_INTEGER result = {{0}};
-    __debugbreak();
-    return result;
+    LARGE_INTEGER product;
+
+    product.QuadPart = (ULONG64)Multiplicand * (ULONG64)Multiplier;
+    return product;
 }
 
 DECLSPEC_DEPRECATED_DDK
@@ -137,8 +151,15 @@ RtlEnlargedUnsignedDivide (
     _Out_opt_ PULONG Remainder
     )
 {
-    __debugbreak();
-    return 0;
+    ULONG quotient;
+
+    quotient = (ULONG)(Dividend.QuadPart / Divisor);
+    if (ARGUMENT_PRESENT(Remainder))
+    {
+        *Remainder = (ULONG)(Dividend.QuadPart % Divisor);
+    }
+
+    return quotient;
 }
 
 DECLSPEC_DEPRECATED_DDK
@@ -150,9 +171,10 @@ RtlExtendedIntegerMultiply (
     _In_ LONG Multiplier
     )
 {
-    LARGE_INTEGER result = {{0}};
-    __debugbreak();
-    return result;
+    LARGE_INTEGER product;
+
+    product.QuadPart = Multiplicand.QuadPart * Multiplier;
+    return product;
 }
 
 DECLSPEC_DEPRECATED_DDK
@@ -164,9 +186,15 @@ RtlExtendedLargeIntegerDivide (
     _In_ ULONG Divisor,
     _Out_opt_ PULONG Remainder)
 {
-    LARGE_INTEGER result = {{0}};
-    __debugbreak();
-    return result;
+    LARGE_INTEGER quotient;
+
+    quotient.QuadPart = (ULONG64)Dividend.QuadPart / Divisor;
+    if (ARGUMENT_PRESENT(Remainder))
+    {
+        *Remainder = (ULONG)((ULONG64)Dividend.QuadPart % Divisor);
+    }
+
+    return quotient;
 }
 
 DECLSPEC_DEPRECATED_DDK
@@ -178,10 +206,27 @@ RtlExtendedMagicDivide (
     _In_ LARGE_INTEGER MagicDivisor,
     _In_ CCHAR ShiftCount)
 {
-    LARGE_INTEGER result = {{0}};
-    __debugbreak();
-    return result;
-}
+    LARGE_INTEGER quotient;
 
+    if (Dividend.QuadPart >= 0)
+    {
+        quotient.QuadPart = UnsignedMultiplyHigh(Dividend.QuadPart,
+                                                 (ULONG64)MagicDivisor.QuadPart);
+
+    }
+    else
+    {
+        quotient.QuadPart = UnsignedMultiplyHigh(-Dividend.QuadPart,
+                                                 (ULONG64)MagicDivisor.QuadPart);
+    }
+
+    quotient.QuadPart = (ULONG64)Quotient.QuadPart >> ShiftCount;
+    if (Dividend.QuadPart < 0)
+    {
+        quotient.QuadPart = - Quotient.QuadPart;
+    }
+
+    return quotient;
+}
 
 } // extern "C"
