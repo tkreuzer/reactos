42d4fb7f475abbbb2a0ef7faa944c85a4112569b
Timo Kreuzer <timo.kreuzer@reactos.org>
2017-12-26 16:10:16 +0100

    [NTDLL_APITEST] Move test for RtlInitilizeBitmap into RtlBitmap.c

--- modules/rostests/apitests/ntdll/RtlBitmap.c
+++ modules/rostests/apitests/ntdll/RtlBitmap.c
@@ -6,6 +6,33 @@
 
 static BOOL IsBroken = FALSE;
 
+void Test_RtlInitializeBitmap()
+{
+    RTL_BITMAP Bitmap;
+    ULONG Buffer[5];
+
+    Buffer[0] = 0x12345;
+    Buffer[1] = 0x23456;
+    Buffer[2] = 0x34567;
+    Buffer[3] = 0x45678;
+    Buffer[4] = 0x56789;
+
+    RtlInitializeBitMap(&Bitmap, Buffer, 19);
+    ok(Bitmap.Buffer == Buffer, "Buffer=%p\n", Bitmap.Buffer);
+    ok(Bitmap.SizeOfBitMap == 19, "SizeOfBitMap=%ld\n", Bitmap.SizeOfBitMap);
+
+    ok(Buffer[0] == 0x12345, "Buffer[0] == 0x%lx\n", Buffer[0]);
+    ok(Buffer[1] == 0x23456, "Buffer[1] == 0x%lx\n", Buffer[1]);
+    ok(Buffer[2] == 0x34567, "Buffer[2] == 0x%lx\n", Buffer[2]);
+    ok(Buffer[3] == 0x45678, "Buffer[3] == 0x%lx\n", Buffer[3]);
+    ok(Buffer[4] == 0x56789, "Buffer[4] == 0x%lx\n", Buffer[4]);
+
+    RtlInitializeBitMap(&Bitmap, 0, -100);
+    ok(Bitmap.Buffer == 0, "Buffer=%p\n", Bitmap.Buffer);
+    ok(Bitmap.SizeOfBitMap == -100, "SizeOfBitMap=%ld\n", Bitmap.SizeOfBitMap);
+
+}
+
 void
 Test_RtlFindMostSignificantBit(void)
 {
@@ -608,6 +635,7 @@ START_TEST(RtlBitmap)
         IsBroken = TRUE;
     }
 
+    Test_RtlInitializeBitmap();
     Test_RtlFindMostSignificantBit();
     Test_RtlFindLeastSignificantBit();
     Test_RtlInitializeBitMap();
