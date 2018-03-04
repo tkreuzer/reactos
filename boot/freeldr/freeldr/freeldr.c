1d1affc71cc95dd1bebb7159ea871fbbe44f4cd7
Timo Kreuzer <timo.kreuzer@reactos.org>
2018-03-04 12:57:50 +0100

    [FREELDR] Add multiboot to MSVC builds (doesn't work yet)

--- boot/freeldr/freeldr/freeldr.c
+++ boot/freeldr/freeldr/freeldr.c
@@ -24,6 +24,11 @@
 #include <debug.h>
 DBG_DEFAULT_CHANNEL(WARNING);
 
+#if defined(_MSC_VER)
+#pragma section(".bss$0",long,read)
+__declspec(allocate(".bss$0")) char _bss_start__;
+#endif
+
 /* GLOBALS ********************************************************************/
 
 #define TOSTRING_(X) #X
