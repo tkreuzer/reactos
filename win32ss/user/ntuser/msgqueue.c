a577667bfea02d4b627abe1a53d1f2f55a46bbd6
Timo Kreuzer <timo.kreuzer@reactos.org>
2018-03-13 23:00:17 +0100

    ? FIXME: [WIN32K] Keep a reference on an ETHREAD while waiting for it. Needs reference from PTIs to ethreads

--- win32ss/user/ntuser/msgqueue.c
+++ win32ss/user/ntuser/msgqueue.c
@@ -1230,6 +1230,9 @@ co_MsqSendMessage(PTHREADINFO ptirec,
       WaitObjects[1] = pti->pEventQueueServer;     // Wait 1
       WaitObjects[2] = ptirec->pEThread;           // Wait 2
 
+      /* We need to take a reference, so it doesn't go away! */
+      //ObReferenceObject(WaitObjects[2]);
+
       do
       {
          UserLeaveCo();
@@ -1279,6 +1282,8 @@ co_MsqSendMessage(PTHREADINFO ptirec,
          while (co_MsqDispatchOneSentMessage(pti))
             ;
       } while (WaitStatus == STATUS_WAIT_1);
+
+      //ObDereferenceObject(WaitObjects[2]);
    }
 
    // Count is nil, restore swapping of the stack.
