882b2df1d3227eaa629e2fa1ae6aee0214dd4a98
Timo Kreuzer <timo.kreuzer@reactos.org>
2018-03-24 11:06:57 +0100

    Additional swapgs related debug code

--- ntoskrnl/ke/amd64/except.c
+++ ntoskrnl/ke/amd64/except.c
@@ -243,7 +243,7 @@ KiPrepareUserDebugData(void)
     _disable();
 }
 
-void __swapgs(void);
+void __swapgs();
 
 VOID
 NTAPI
@@ -254,10 +254,20 @@ KiDispatchException(IN PEXCEPTION_RECORD ExceptionRecord,
                     IN BOOLEAN FirstChance)
 {
     CONTEXT Context;
+    PKIPCR Pcr;
 
-    if ((PVOID)KeGetCurrentPrcb() < MmSystemRangeStart)
+    if ((PVOID)__readmsr(MSR_GS_BASE) < MmSystemRangeStart)
     {
         __swapgs();
+        __debugbreak();
+    }
+
+    Pcr = (PKIPCR)KeGetPcr();
+    if ((PVOID)Pcr < MmSystemRangeStart)
+    {
+        Pcr = (PKIPCR)__readmsr(MSR_GS_BASE);
+        Pcr->Self = (PKPCR)Pcr;
+        Pcr->CurrentPrcb = &Pcr->Prcb;
         __debugbreak();
     }
 
@@ -661,7 +671,7 @@ KiGeneralProtectionFaultHandler(
         TrapFrame->SegEs = (KGDT64_R3_DATA | RPL_MASK);
         return STATUS_SUCCESS;
     }
-
+    __debugbreak();
     /* Get Instruction Pointer */
     Instructions = (PUCHAR)TrapFrame->Rip;
 
