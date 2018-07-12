5c686e78d3a38a2a52d53f5835054868665888d9
Timo Kreuzer <timo.kreuzer@reactos.org>
2018-07-12 14:24:59 +0200

    !! More wrong pathes

--- sdk/lib/crt/startup/dllmain.c
+++ sdk/lib/crt/startup/dllmain.c
@@ -2,10 +2,7 @@
 #define _DECL_DLLMAIN
 #include <process.h>
 
-WINBOOL WINAPI DllMain (HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
+BOOL WINAPI DllMain(HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
 {
-    /* If the DLL provides no DllMain, then chances are that it doesn't bother with thread initialization */
-    if(dwReason == DLL_PROCESS_ATTACH)
-        DisableThreadLibraryCalls(hDllHandle);
-    return TRUE;
+  return TRUE;
 }
