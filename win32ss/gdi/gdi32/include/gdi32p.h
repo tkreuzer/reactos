eedd2d17f507dea598ff88ea92de737c09511f85
Timo Kreuzer <timo.kreuzer@reactos.org>
2018-08-19 15:31:23 +0200

    Bring back PathToRegion It is unimplemented, but should not simply be removed.

--- win32ss/gdi/gdi32/include/gdi32p.h
+++ win32ss/gdi/gdi32/include/gdi32p.h
@@ -659,6 +659,7 @@ typedef enum _DCFUNC
     DCFUNC_OffsetClipRgn,
     DCFUNC_OffsetViewportOrgEx,
     DCFUNC_OffsetWindowOrgEx,
+    DCFUNC_PathToRegion, // UNIMPLEMENTED
     DCFUNC_PatBlt,
     DCFUNC_Pie,
     DCFUNC_PlgBlt, // UNIMPLEMENTED
