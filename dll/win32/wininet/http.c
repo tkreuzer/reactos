71cdd8bb106dced6b20fcfa03bba98d057516f1d
Timo Kreuzer <timo.kreuzer@reactos.org>
2019-05-25 12:45:03 +0200

    HACK [REACTOS] Fix warnings about different const qualifiers

--- dll/win32/wininet/http.c
+++ dll/win32/wininet/http.c
@@ -605,7 +605,7 @@ static WCHAR* build_request_header(http_request_t *request, const WCHAR *verb,
     req[n] = NULL;
 
     requestString = HTTP_build_req( req, 4 );
-    heap_free( req );
+    heap_free( (void*)req );
     LeaveCriticalSection( &request->headers_section );
     return requestString;
 }
@@ -658,7 +658,7 @@ static WCHAR* build_response_header(http_request_t *request, BOOL use_cr)
     req[n] = NULL;
 
     ret = HTTP_build_req(req, 0);
-    heap_free(req);
+    heap_free((void*)req);
     LeaveCriticalSection( &request->headers_section );
     return ret;
 }
