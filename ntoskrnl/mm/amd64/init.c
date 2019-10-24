9c7e239fc36ee79adf7c8ad04ed5c7995ae88fd0
Timo Kreuzer <timo.kreuzer@reactos.org>
2019-10-24 09:44:10 +0200

    [NTOS:Mm] Improve x64 address space initialization
    * Move MiBuildPfnDatabaseFromPageTables into MiBuildPfnDatabase
    * On x64 don't initialize page table PFNs for the initial process in MmInitializeProcessAddressSpace as they are already set up now

--- ntoskrnl/mm/amd64/init.c
+++ ntoskrnl/mm/amd64/init.c
@@ -438,8 +438,9 @@ MiSetupPfnForPageTable(
     Pfn->u2.ShareCount++;
 }
 
+static
 VOID
-NTAPI
+INIT_FUNCTION
 MiBuildPfnDatabaseFromPageTables(VOID)
 {
     PVOID Address = NULL;
@@ -538,8 +539,8 @@ MiBuildPfnDatabaseFromPageTables(VOID)
 #endif
 }
 
+static
 VOID
-NTAPI
 INIT_FUNCTION
 MiAddDescriptorToDatabase(
     PFN_NUMBER BasePage,
@@ -560,7 +561,7 @@ MiAddDescriptorToDatabase(
         while (PageCount--)
         {
             /* Add it to the free list */
-            Pfn->u3.e1.CacheAttribute = MiNonCached;
+            Pfn->u3.e1.CacheAttribute = MiNonCached; // FIXME: Windows ASSERTs MiChached, but why not MiNotMapped?
             MiInsertPageInFreeList(BasePage + PageCount);
 
             /* Go to the previous page */
@@ -673,6 +674,12 @@ MiBuildPfnDatabase(IN PLOADER_PARAMETER_BLOCK LoaderBlock)
 
     /* Reset the descriptor back so we can create the correct memory blocks */
     *MxFreeDescriptor = MxOldFreeDescriptor;
+
+    /* Now process the page tables */
+    MiBuildPfnDatabaseFromPageTables();
+
+    /* PFNs are initialized now! */
+    MiPfnsInitialized = TRUE;
 }
 
 NTSTATUS
@@ -726,17 +733,6 @@ MiInitMachineDependent(IN PLOADER_PARAMETER_BLOCK LoaderBlock)
         DPRINT1("MmInitializeProcessAddressSpace(9 failed: 0x%lx\n", Status);
         return Status;
     }
-
-    /* Need to be at DISPATCH_LEVEL for MiBuildPfnDatabaseFromPageTables */
-    KeRaiseIrql(DISPATCH_LEVEL, &OldIrql);
-
-    /* Now process the page tables */
-    MiBuildPfnDatabaseFromPageTables();
-
-    /* PFNs are initialized now! */
-    MiPfnsInitialized = TRUE;
-
-    KeLowerIrql(OldIrql);
 
     /* Initialize the balancer */
     MmInitializeBalancer((ULONG)MmAvailablePages, 0);
