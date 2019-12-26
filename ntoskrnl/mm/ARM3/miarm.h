9b27508ec6396f32b7a28a655d1fef7de1645153
Timo Kreuzer <timo.kreuzer@reactos.org>
2019-12-26 20:30:54 +0100

    [NTOS] Implement x64 specific mechanism for counting page table references

--- ntoskrnl/mm/ARM3/miarm.h
+++ ntoskrnl/mm/ARM3/miarm.h
@@ -738,6 +738,22 @@ MiIsUserPte(PVOID Address)
 }
 #endif
 
+FORCEINLINE
+BOOLEAN
+MiIsUserAddressOrPageTable(PVOID Address)
+{
+    return ((Address <= MmHighestUserAddress) // MM_HIGHEST_USER_ADDRESS
+            || MiIsUserPte(Address)
+            || MiIsUserPde(Address)
+#if (_MI_PAGING_LEVELS >= 3)
+            || MiIsUserPpe(Address)
+#endif
+#if (_MI_PAGING_LEVELS >= 4)
+            || MiIsUserPxe(Address)
+#endif
+            );
+}
+
 //
 // Figures out the hardware bits for a PTE
 //
@@ -1679,6 +1695,50 @@ MiReferenceUnusedPageAndBumpLockCount(IN PMMPFN Pfn1)
     }
 }
 
+#ifdef _M_AMD64
+FORCEINLINE
+VOID
+MiIncrementPageTableReferences(IN PVOID Address)
+{
+    PULONG RefCount;
+    PMMPDE PointerPde = MiAddressToPde(Address);
+
+    NT_ASSERT(PointerPde->u.Hard.Valid);
+    RefCount = &MI_PFN_ELEMENT(PointerPde->u.Hard.PageFrameNumber)->UsedPageTableEntries;
+
+    *RefCount += 1;
+    ASSERT(*RefCount <= PTE_PER_PAGE);
+}
+
+FORCEINLINE
+USHORT
+MiDecrementPageTableReferences(IN PVOID Address)
+{
+    PULONG RefCount;
+    PMMPDE PointerPde = MiAddressToPde(Address);
+
+    NT_ASSERT(PointerPde->u.Hard.Valid);
+    RefCount = &MI_PFN_ELEMENT(PointerPde->u.Hard.PageFrameNumber)->UsedPageTableEntries;
+
+    *RefCount -= 1;
+    ASSERT(*RefCount < PTE_PER_PAGE);
+
+    return *RefCount;
+}
+
+FORCEINLINE
+USHORT
+MiQueryPageTableReferences(IN PVOID Address)
+{
+    PULONG RefCount;
+    PMMPTE PointerPte = MiAddressToPde(Address);
+
+    NT_ASSERT(PointerPte->u.Hard.Valid);
+    RefCount = &MI_PFN_ELEMENT(PointerPte->u.Hard.PageFrameNumber)->UsedPageTableEntries;
+
+    return *RefCount;
+}
+#else
 FORCEINLINE
 VOID
 MiIncrementPageTableReferences(IN PVOID Address)
@@ -1692,7 +1752,7 @@ MiIncrementPageTableReferences(IN PVOID Address)
 }
 
 FORCEINLINE
-VOID
+USHORT
 MiDecrementPageTableReferences(IN PVOID Address)
 {
     PUSHORT RefCount;
@@ -1701,6 +1761,7 @@ MiDecrementPageTableReferences(IN PVOID Address)
 
     *RefCount -= 1;
     ASSERT(*RefCount < PTE_PER_PAGE);
+    return *RefCount;
 }
 
 FORCEINLINE
@@ -1713,6 +1774,7 @@ MiQueryPageTableReferences(IN PVOID Address)
 
     return *RefCount;
 }
+#endif
 
 INIT_FUNCTION
 BOOLEAN
