7e44d0328d16dbb263bf695ab5fe5eadc2c39acb
Timo Kreuzer <timo.kreuzer@reactos.org>
2020-02-29 14:16:42 +0100

    fix fix advapi32_vista

--- dll/win32/advapi32/DllMainStub.c
+++ dll/win32/advapi32/DllMainStub.c
@@ -1,11 +1,6 @@
 
-/* PSDK/NDK Headers */
 #define WIN32_NO_STATUS
 #include <windef.h>
-#include <winbase.h>
-#include <winreg.h>
-
-#include <ndk/rtlfuncs.h>
 
 BOOL
 WINAPI
@@ -13,8 +8,5 @@ DllMain(HANDLE hDll,
         DWORD dwReason,
         LPVOID lpReserved)
 {
-    /* For now, there isn't much to do */
-    if (dwReason == DLL_PROCESS_ATTACH)
-        DisableThreadLibraryCalls(hDll);
     return TRUE;
 }
