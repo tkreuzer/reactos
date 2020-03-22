fe251ad1cf3ac0e2f116a727949ae870d07fdaad
Timo Kreuzer <timo.kreuzer@reactos.org>
2020-03-22 14:10:25 +0100

    debug c++ fail

--- modules/rostests/apitests/compiler/pseh.c
+++ modules/rostests/apitests/compiler/pseh.c
@@ -2581,7 +2581,7 @@ DEFINE_TEST(test_nested_exception)
         _SEH2_END;
 
         val |= 32;
-        *((char*)(intptr_t)0xbaadc0de) = 0;
+        *((char*)(intptr_t)0xdeadc0de) = 0;
         val |= 64;
     }
     _SEH2_EXCEPT(1)
@@ -2762,7 +2762,7 @@ struct subtest
 
 START_TEST(pseh)
 {
-	const struct subtest testsuite[] =
+	const struct subtest testsuite_[] =
 	{
 		USE_TEST(test_empty_1),
 		USE_TEST(test_empty_2),
@@ -2887,8 +2887,136 @@ START_TEST(pseh)
 		USE_TEST(test_PSEH3_bug2),
 	};
 
-	size_t i;
+	const struct subtest testsuite[] =
+	{
 
+		USE_TEST(test_empty_1),
+		USE_TEST(test_empty_2),
+		USE_TEST(test_empty_3),
+		USE_TEST(test_empty_4),
+		USE_TEST(test_empty_5),
+		USE_TEST(test_empty_6),
+		USE_TEST(test_empty_7),
+		USE_TEST(test_empty_8),
+
+		USE_TEST(test_execute_handler_1),
+#if 0
+		USE_TEST(test_continue_execution_1),
+		USE_TEST(test_continue_search_1),
+		USE_TEST(test_execute_handler_2),
+		USE_TEST(test_continue_execution_2),
+
+		USE_TEST(test_execute_handler_3),
+		USE_TEST(test_continue_execution_3),
+		USE_TEST(test_continue_search_2),
+		USE_TEST(test_execute_handler_4),
+		USE_TEST(test_continue_execution_4),
+
+		USE_TEST(test_execute_handler_5),
+		USE_TEST(test_continue_execution_5),
+		USE_TEST(test_continue_search_3),
+		USE_TEST(test_execute_handler_6),
+		USE_TEST(test_continue_execution_6),
+
+		USE_TEST(test_execute_handler_7),
+		USE_TEST(test_continue_execution_7),
+		USE_TEST(test_continue_search_4),
+		USE_TEST(test_execute_handler_8),
+		USE_TEST(test_continue_execution_8),
+
+		USE_TEST(test_execute_handler_9),
+		USE_TEST(test_continue_execution_9),
+		USE_TEST(test_continue_search_5),
+		USE_TEST(test_execute_handler_10),
+		USE_TEST(test_continue_execution_10),
+
+		USE_TEST(test_execute_handler_11),
+		USE_TEST(test_continue_execution_11),
+		USE_TEST(test_continue_search_6),
+		USE_TEST(test_execute_handler_12),
+		USE_TEST(test_continue_execution_12),
+
+		USE_TEST(test_leave_1),
+		USE_TEST(test_leave_2),
+		USE_TEST(test_leave_3),
+		USE_TEST(test_leave_4),
+		USE_TEST(test_leave_5),
+		USE_TEST(test_leave_6),
+
+		USE_TEST(test_yield_1),
+		USE_TEST(test_yield_2),
+		USE_TEST(test_yield_3),
+		USE_TEST(test_yield_4),
+		USE_TEST(test_yield_5),
+		USE_TEST(test_yield_6),
+
+		USE_TEST(test_finally_1),
+		USE_TEST(test_finally_2),
+		USE_TEST(test_finally_3),
+		USE_TEST(test_finally_4),
+		USE_TEST(test_finally_5),
+		USE_TEST(test_finally_6),
+		USE_TEST(test_finally_7),
+		USE_TEST(test_finally_8),
+		USE_TEST(test_finally_9),
+		USE_TEST(test_finally_10),
+		USE_TEST(test_finally_11),
+		USE_TEST(test_finally_12),
+		USE_TEST(test_finally_13),
+		USE_TEST(test_finally_14),
+
+		USE_TEST(test_xpointers_1),
+		USE_TEST(test_xpointers_2),
+		USE_TEST(test_xpointers_3),
+		USE_TEST(test_xpointers_4),
+		USE_TEST(test_xpointers_5),
+		USE_TEST(test_xpointers_6),
+		USE_TEST(test_xpointers_7),
+		USE_TEST(test_xpointers_8),
+		USE_TEST(test_xpointers_9),
+		USE_TEST(test_xpointers_10),
+		USE_TEST(test_xpointers_11),
+		USE_TEST(test_xpointers_12),
+		USE_TEST(test_xpointers_13),
+		USE_TEST(test_xpointers_14),
+		USE_TEST(test_xpointers_15),
+		USE_TEST(test_xpointers_16),
+
+		USE_TEST(test_xcode_1),
+		USE_TEST(test_xcode_2),
+		USE_TEST(test_xcode_3),
+
+		USE_TEST(test_abnorm_1),
+		USE_TEST(test_abnorm_2),
+		USE_TEST(test_abnorm_3),
+		USE_TEST(test_abnorm_4),
+		USE_TEST(test_abnorm_5),
+		USE_TEST(test_abnorm_6),
+		USE_TEST(test_abnorm_7),
+		USE_TEST(test_abnorm_8),
+
+		USE_TEST(test_nested_locals_1),
+		USE_TEST(test_nested_locals_2),
+		USE_TEST(test_nested_locals_3),
+
+		USE_TEST(test_bug_4004),
+		USE_TEST(test_bug_4663),
+
+		USE_TEST(test_unvolatile),
+		USE_TEST(test_unvolatile_2),
+#ifndef __cplusplus
+		USE_TEST(test_unvolatile_3),
+#endif
+		USE_TEST(test_unvolatile_4),
+		USE_TEST(test_finally_goto),
+		USE_TEST(test_nested_exception),
+		USE_TEST(test_PSEH3_bug),
+		USE_TEST(test_PSEH3_bug2),
+#endif
+	};
+
+	size_t i;
+	(void)testsuite_;
 	for(i = 0; i < sizeof(testsuite) / sizeof(testsuite[0]); ++ i)
 		ok(call_test(testsuite[i].func), "%s failed\n", testsuite[i].name);
 }
