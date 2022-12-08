wf61694/5

[REACTOS] Fix MSVC printf format warnings

base/services/nfsd/daemon_debug.c
@@ -228,45 +228,45 @@ void print_share_mode(int level, DWORD mode)
 void print_file_id_both_dir_info(int level, FILE_ID_BOTH_DIR_INFO *pboth_dir_info)
 {
     if (level > g_debug_level) return;
-    fprintf(dlog_file, "FILE_ID_BOTH_DIR_INFO %p %d\n", 
+    fprintf(dlog_file, "FILE_ID_BOTH_DIR_INFO %p %zd\n", 
        pboth_dir_info, sizeof(unsigned char *));
-    fprintf(dlog_file, "\tNextEntryOffset=%ld %d %d\n", 
+    fprintf(dlog_file, "\tNextEntryOffset=%ld %zd %zd\n", 
         pboth_dir_info->NextEntryOffset, 
         sizeof(pboth_dir_info->NextEntryOffset), sizeof(DWORD));
-    fprintf(dlog_file, "\tFileIndex=%ld  %d\n", pboth_dir_info->FileIndex, 
+    fprintf(dlog_file, "\tFileIndex=%ld  %zd\n", pboth_dir_info->FileIndex, 
         sizeof(pboth_dir_info->FileIndex));
-    fprintf(dlog_file, "\tCreationTime=0x%x %d\n", 
+    fprintf(dlog_file, "\tCreationTime=0x%llx %zd\n", 
         pboth_dir_info->CreationTime.QuadPart, 
         sizeof(pboth_dir_info->CreationTime));
-    fprintf(dlog_file, "\tLastAccessTime=0x%x %d\n", 
+    fprintf(dlog_file, "\tLastAccessTime=0x%llx %zd\n", 
         pboth_dir_info->LastAccessTime.QuadPart, 
         sizeof(pboth_dir_info->LastAccessTime));
-    fprintf(dlog_file, "\tLastWriteTime=0x%x %d\n", 
+    fprintf(dlog_file, "\tLastWriteTime=0x%llx %zd\n", 
         pboth_dir_info->LastWriteTime.QuadPart, 
         sizeof(pboth_dir_info->LastWriteTime));
-    fprintf(dlog_file, "\tChangeTime=0x%x %d\n", 
+    fprintf(dlog_file, "\tChangeTime=0x%llx %zd\n", 
         pboth_dir_info->ChangeTime.QuadPart, 
         sizeof(pboth_dir_info->ChangeTime));
-    fprintf(dlog_file, "\tEndOfFile=0x%x %d\n", 
+    fprintf(dlog_file, "\tEndOfFile=0x%llx %zd\n", 
         pboth_dir_info->EndOfFile.QuadPart, 
         sizeof(pboth_dir_info->EndOfFile));
-    fprintf(dlog_file, "\tAllocationSize=0x%x %d\n", 
+    fprintf(dlog_file, "\tAllocationSize=0x%llx %zd\n", 
         pboth_dir_info->AllocationSize.QuadPart, 
         sizeof(pboth_dir_info->AllocationSize));
-    fprintf(dlog_file, "\tFileAttributes=%ld %d\n", 
+    fprintf(dlog_file, "\tFileAttributes=%ld %zd\n", 
         pboth_dir_info->FileAttributes, 
         sizeof(pboth_dir_info->FileAttributes));
-    fprintf(dlog_file, "\tFileNameLength=%ld %d\n", 
+    fprintf(dlog_file, "\tFileNameLength=%ld %zd\n", 
         pboth_dir_info->FileNameLength, 
         sizeof(pboth_dir_info->FileNameLength));
-    fprintf(dlog_file, "\tEaSize=%ld %d\n", 
+    fprintf(dlog_file, "\tEaSize=%ld %zd\n", 
         pboth_dir_info->EaSize, sizeof(pboth_dir_info->EaSize));
-    fprintf(dlog_file, "\tShortNameLength=%d %d\n", 
+    fprintf(dlog_file, "\tShortNameLength=%d %zd\n", 
         pboth_dir_info->ShortNameLength, 
         sizeof(pboth_dir_info->ShortNameLength));
-    fprintf(dlog_file, "\tShortName='%S' %d\n", pboth_dir_info->ShortName, 
+    fprintf(dlog_file, "\tShortName='%S' %zd\n", pboth_dir_info->ShortName, 
         sizeof(pboth_dir_info->ShortName));
-    fprintf(dlog_file, "\tFileId=0x%x %d\n", pboth_dir_info->FileId.QuadPart, 
+    fprintf(dlog_file, "\tFileId=0x%llx %zd\n", pboth_dir_info->FileId.QuadPart, 
         sizeof(pboth_dir_info->FileId));
     fprintf(dlog_file, "\tFileName='%S' %p\n", pboth_dir_info->FileName, 
         pboth_dir_info->FileName);

dll/3rdparty/libtirpc/src/clnt_vc.c
@@ -486,8 +486,8 @@ clnt_vc_create(fd, raddr, prog, vers, sendsz, recvsz, cb_xdr, cb_fn, cb_args)
             fprintf(stderr, "_beginthreadex failed %d\n", GetLastError());
             goto err;
         } else
-            fprintf(stdout, "%04x: started the callback thread %04x\n", 
-                GetCurrentThreadId(), cl->cb_thread);
+            fprintf(stdout, "%04x: started the callback thread %04zx\n", 
+                GetCurrentThreadId(), (ULONG_PTR)cl->cb_thread);
     } else
         cl->cb_thread = INVALID_HANDLE_VALUE;
 	return (cl);
@@ -921,8 +921,8 @@ clnt_vc_destroy(cl)
 
     if (cl->cb_thread != INVALID_HANDLE_VALUE) {
         int status;
-        fprintf(stdout, "%04x: sending shutdown to callback thread %04x\n", 
-            GetCurrentThreadId(), cl->cb_thread);
+        fprintf(stdout, "%04x: sending shutdown to callback thread %04zx\n", 
+            GetCurrentThreadId(), (ULONG_PTR)cl->cb_thread);
         cl->shutdown = 1;
         mutex_unlock(&clnt_fd_lock);
         cond_signal(&vc_cv[WINSOCK_HANDLE_HASH(ct_fd)]);

dll/win32/comctl32/listview.c
@@ -645,7 +645,7 @@ static const char* debuglvitem_t(const LVITEMW *lpLVItem, BOOL isW)
     if (len == -1) goto end;
     buf += len; size -= len;
     if (lpLVItem->mask & LVIF_PARAM)
-	len = snprintf(buf, size, "lParam=%lx, ", lpLVItem->lParam);
+	len = snprintf(buf, size, "lParam=%zx, ", lpLVItem->lParam);
     else len = 0;
     if (len == -1) goto end;
     buf += len; size -= len;

dll/win32/dbghelp/msc.c
@@ -3083,7 +3083,7 @@ static BOOL  pev_binop(struct pevaluator* pev, char op)
     case '%': c = v1 % v2; break;
     default: return PEV_ERROR1(pev, "binop: unknown op (%c)", op);
     }
-    snprintf(res, sizeof(res), "%ld", c);
+    snprintf(res, sizeof(res), "%zd", c);
     pev_push(pev, res);
     return TRUE;
 }
@@ -3096,8 +3096,8 @@ static BOOL  pev_deref(struct pevaluator* pev)
 
     if (!pev_pop_val(pev, &v1)) return FALSE;
     if (!sw_read_mem(pev->csw, v1, &v2, sizeof(v2)))
-        return PEV_ERROR1(pev, "deref: cannot read mem at %lx\n", v1);
-    snprintf(res, sizeof(res), "%ld", v2);
+        return PEV_ERROR1(pev, "deref: cannot read mem at %zx\n", v1);
+    snprintf(res, sizeof(res), "%zd", v2);
     pev_push(pev, res);
     return TRUE;
 }

tkreuzer/reactos

base/services/nfsd/daemon_debug.c
@@ -228,45 +228,45 @@ void print_share_mode(int level, DWORD mode)
 void print_file_id_both_dir_info(int level, FILE_ID_BOTH_DIR_INFO *pboth_dir_info)
 {
     if (level > g_debug_level) return;
-    fprintf(dlog_file, "FILE_ID_BOTH_DIR_INFO %p %d\n", 
+    fprintf(dlog_file, "FILE_ID_BOTH_DIR_INFO %p %Id\n", 
        pboth_dir_info, sizeof(unsigned char *));
-    fprintf(dlog_file, "\tNextEntryOffset=%ld %d %d\n", 
+    fprintf(dlog_file, "\tNextEntryOffset=%ld %Id %Id\n", 
         pboth_dir_info->NextEntryOffset, 
         sizeof(pboth_dir_info->NextEntryOffset), sizeof(DWORD));
-    fprintf(dlog_file, "\tFileIndex=%ld  %d\n", pboth_dir_info->FileIndex, 
+    fprintf(dlog_file, "\tFileIndex=%ld  %Id\n", pboth_dir_info->FileIndex, 
         sizeof(pboth_dir_info->FileIndex));
-    fprintf(dlog_file, "\tCreationTime=0x%x %d\n", 
+    fprintf(dlog_file, "\tCreationTime=0x%llx %Id\n", 
         pboth_dir_info->CreationTime.QuadPart, 
         sizeof(pboth_dir_info->CreationTime));
-    fprintf(dlog_file, "\tLastAccessTime=0x%x %d\n", 
+    fprintf(dlog_file, "\tLastAccessTime=0x%llx %Id\n", 
         pboth_dir_info->LastAccessTime.QuadPart, 
         sizeof(pboth_dir_info->LastAccessTime));
-    fprintf(dlog_file, "\tLastWriteTime=0x%x %d\n", 
+    fprintf(dlog_file, "\tLastWriteTime=0x%llx %Id\n", 
         pboth_dir_info->LastWriteTime.QuadPart, 
         sizeof(pboth_dir_info->LastWriteTime));
-    fprintf(dlog_file, "\tChangeTime=0x%x %d\n", 
+    fprintf(dlog_file, "\tChangeTime=0x%llx %Id\n", 
         pboth_dir_info->ChangeTime.QuadPart, 
         sizeof(pboth_dir_info->ChangeTime));
-    fprintf(dlog_file, "\tEndOfFile=0x%x %d\n", 
+    fprintf(dlog_file, "\tEndOfFile=0x%llx %Id\n", 
         pboth_dir_info->EndOfFile.QuadPart, 
         sizeof(pboth_dir_info->EndOfFile));
-    fprintf(dlog_file, "\tAllocationSize=0x%x %d\n", 
+    fprintf(dlog_file, "\tAllocationSize=0x%llx %Id\n", 
         pboth_dir_info->AllocationSize.QuadPart, 
         sizeof(pboth_dir_info->AllocationSize));
-    fprintf(dlog_file, "\tFileAttributes=%ld %d\n", 
+    fprintf(dlog_file, "\tFileAttributes=%ld %Id\n", 
         pboth_dir_info->FileAttributes, 
         sizeof(pboth_dir_info->FileAttributes));
-    fprintf(dlog_file, "\tFileNameLength=%ld %d\n", 
+    fprintf(dlog_file, "\tFileNameLength=%ld %Id\n", 
         pboth_dir_info->FileNameLength, 
         sizeof(pboth_dir_info->FileNameLength));
-    fprintf(dlog_file, "\tEaSize=%ld %d\n", 
+    fprintf(dlog_file, "\tEaSize=%ld %Id\n", 
         pboth_dir_info->EaSize, sizeof(pboth_dir_info->EaSize));
-    fprintf(dlog_file, "\tShortNameLength=%d %d\n", 
+    fprintf(dlog_file, "\tShortNameLength=%d %Id\n", 
         pboth_dir_info->ShortNameLength, 
         sizeof(pboth_dir_info->ShortNameLength));
-    fprintf(dlog_file, "\tShortName='%S' %d\n", pboth_dir_info->ShortName, 
+    fprintf(dlog_file, "\tShortName='%S' %Id\n", pboth_dir_info->ShortName, 
         sizeof(pboth_dir_info->ShortName));
-    fprintf(dlog_file, "\tFileId=0x%x %d\n", pboth_dir_info->FileId.QuadPart, 
+    fprintf(dlog_file, "\tFileId=0x%llx %Id\n", pboth_dir_info->FileId.QuadPart, 
         sizeof(pboth_dir_info->FileId));
     fprintf(dlog_file, "\tFileName='%S' %p\n", pboth_dir_info->FileName, 
         pboth_dir_info->FileName);

dll/3rdparty/libtirpc/src/clnt_vc.c
@@ -486,8 +486,8 @@ clnt_vc_create(fd, raddr, prog, vers, sendsz, recvsz, cb_xdr, cb_fn, cb_args)
             fprintf(stderr, "_beginthreadex failed %d\n", GetLastError());
             goto err;
         } else
-            fprintf(stdout, "%04x: started the callback thread %04x\n", 
-                GetCurrentThreadId(), cl->cb_thread);
+            fprintf(stdout, "%04x: started the callback thread %04Ix\n", 
+                GetCurrentThreadId(), (ULONG_PTR)cl->cb_thread);
     } else
         cl->cb_thread = INVALID_HANDLE_VALUE;
 	return (cl);
@@ -921,8 +921,8 @@ clnt_vc_destroy(cl)
 
     if (cl->cb_thread != INVALID_HANDLE_VALUE) {
         int status;
-        fprintf(stdout, "%04x: sending shutdown to callback thread %04x\n", 
-            GetCurrentThreadId(), cl->cb_thread);
+        fprintf(stdout, "%04x: sending shutdown to callback thread %04Ix\n", 
+            GetCurrentThreadId(), (ULONG_PTR)cl->cb_thread);
         cl->shutdown = 1;
         mutex_unlock(&clnt_fd_lock);
         cond_signal(&vc_cv[WINSOCK_HANDLE_HASH(ct_fd)]);

dll/win32/comctl32/listview.c
@@ -648,7 +648,7 @@ static const char* debuglvitem_t(const LVITEMW *lpLVItem, BOOL isW)
     if (len == -1) goto end;
     buf += len; size -= len;
     if (lpLVItem->mask & LVIF_PARAM)
-	len = snprintf(buf, size, "lParam=%lx, ", lpLVItem->lParam);
+	len = snprintf(buf, size, "lParam=%Ix, ", lpLVItem->lParam);
     else len = 0;
     if (len == -1) goto end;
     buf += len; size -= len;

dll/win32/dbghelp/msc.c
@@ -3081,7 +3081,7 @@ static BOOL  pev_binop(struct pevaluator* pev, char op)
     case '%': c = v1 % v2; break;
     default: return PEV_ERROR1(pev, "binop: unknown op (%c)", op);
     }
-    snprintf(res, sizeof(res), "%ld", c);
+    snprintf(res, sizeof(res), "%Id", c);
     pev_push(pev, res);
     return TRUE;
 }
@@ -3094,8 +3094,8 @@ static BOOL  pev_deref(struct pevaluator* pev)
 
     if (!pev_pop_val(pev, &v1)) return FALSE;
     if (!sw_read_mem(pev->csw, v1, &v2, pev->csw->cpu->word_size))
-        return PEV_ERROR1(pev, "deref: cannot read mem at %lx\n", v1);
-    snprintf(res, sizeof(res), "%ld", v2);
+        return PEV_ERROR1(pev, "deref: cannot read mem at %Ix\n", v1);
+    snprintf(res, sizeof(res), "%Id", v2);
     pev_push(pev, res);
     return TRUE;
 }