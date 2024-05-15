4154ca566587a3deb86a69da8ef30c95a2519e75
Timo Kreuzer <timo.kreuzer@reactos.org>
2024-05-15 19:16:07 +0300

    [KMTEST] Handle dummy definition of _iobuf structure

--- modules/rostests/kmtests/kmtest_drv/printf_stubs.c
+++ modules/rostests/kmtests/kmtest_drv/printf_stubs.c
@@ -6,6 +6,20 @@
  */
 
 #undef wctomb
+
+  struct _iobuf {
+    char *_ptr;
+    int _cnt;
+    char *_base;
+    int _flag;
+    int _file;
+    int _charbuf;
+    int _bufsiz;
+    char *_tmpfname;
+  };
+  typedef struct _iobuf FILE;
+#define _FILE_DEFINED
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <wchar.h>
