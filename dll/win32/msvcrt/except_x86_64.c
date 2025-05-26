90365b2d1f388227cc03251adf5d1862f2e51a29
Timo Kreuzer <timo.kreuzer@reactos.org>
2025-05-26 10:38:50 +0300

    WIP [UCRTSUPPORT]/[MSVCRT_SHARED] Use msvcrt for ucrtsupport library
    - Rename to UCRTSUPPORT

--- dll/win32/msvcrt/except_x86_64.c
+++ dll/win32/msvcrt/except_x86_64.c
@@ -92,6 +92,7 @@ ULONG_PTR get_exception_pc( DISPATCHER_CONTEXT *dispatch )
  *		longjmp (MSVCRT.@)
  */
 #ifndef __WINE_PE_BUILD
+#ifndef __UCRTSUPPORT__
 void __cdecl longjmp( _JUMP_BUFFER *jmp, int retval )
 {
     EXCEPTION_RECORD rec;
@@ -109,6 +110,7 @@ void __cdecl longjmp( _JUMP_BUFFER *jmp, int retval )
     }
     __wine_longjmp( (__wine_jmp_buf *)jmp, retval );
 }
+#endif /* __UCRTSUPPORT__ */
 #endif
 #endif // __REACTOS__
 
